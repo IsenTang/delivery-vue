786a1f47b0d2dce8847702fa9ea16057abb70927
isenTang <3104255643@qq.com>
2020-02-15 23:26:11 +0800

    login go back login

--- src/Features/Login/module/actions.js
+++ src/Features/Login/module/actions.js
@@ -2,27 +2,33 @@ import { checkLogin } from '../../../Requests/login';
 import { encode } from '../../../Services/login';
 import * as types from '../../../Store/mutation-types';
 import { set } from '../../../Common/utils';
+import router from '../../../Router';
 
 const actions = {
 
    async login ({ commit },{ username,password }){
 
-      commit(types.SHOW_LOADING,null,{ root: true });
+      commit(types.SHOW_LOADING);
 
       try {
          /* check login */
          const user = await checkLogin({ username:encode(username), password:encode(password) });
 
-         console.log(user);
          commit(types.SAVE_USER,{ user });
 
          set('user',user);
+
+         if(history.length > 1){
+            router.back();
+         }else{
+            router.push('/restaurant');
+         }
 
       } catch (error) {
 
          this._vm.$modal.show('modal',{ message: error.message });
       }finally{
-         commit(types.HIDE_LOADING,null,{ root: true });
+         commit(types.HIDE_LOADING);
       }
    }
 };
